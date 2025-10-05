bd6e5a305f570b9e5f7c8e64d764cd28d2d4b568
zsheikhnajdi <118489106+zsheikhnajdi@users.noreply.github.com>
2025-10-05 18:52:20 +0200

    Delete data/sample/50row.py

--- data/sample/50row.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv("data/raw/pima-indians-diabetes.data.csv", header=None)
-df.head(50).to_csv("data/sample/pima-indians-diabetes.data.csv", index=False, header=False)
